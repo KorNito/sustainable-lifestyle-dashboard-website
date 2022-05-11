350a9358601598469567f2d9bed053e941cd9fac
KorNito <kornelijuspacevicius@gmail.com>
2022-05-11 15:38:49 +0300

    Added navbar display element logic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,91 @@
 import React, { useState } from "react";
-import classes from "./Navbar.css";
-import { Link } from "react-router-dom";
-import { Button } from "./Button";
-import Dropdown from "./Dropdown";
+import { Outlet, Link } from "react-router-dom";
+import "./Navbar.css";
+import ChallengesDropdown from "./ChallengesDropdown";
+import RewardsDropdown from "./RewardsDropdown";
 
 function Navbar() {
-  const [menuOpened, setMenuOpened] = useState(false);
+  const [click, setClick] = useState(false);
+  const [challengesDropdown, setChallengesDropdown] = useState(false);
+  const [rewardsDropdown, setRewardsDropdown] = useState(false);
 
-  const handleClick = () => setMenuOpened(!menuOpened);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const onMouseChallengesEnter = () => {
+    if (window.innerWidth < 960) {
+      setChallengesDropdown(false);
+    } else {
+      setChallengesDropdown(true);
+    }
+  };
+
+  const onMouseChallengesLeave = () => {
+    if (window.innerWidth < 960) {
+      setChallengesDropdown(false);
+    } else {
+      setChallengesDropdown(false);
+    }
+  };
+
+  const onMouseRewardsEnter = () => {
+    if (window.innerWidth < 960) {
+      setRewardsDropdown(false);
+    } else {
+      setRewardsDropdown(true);
+    }
+  };
+
+  const onMouseRewardsLeave = () => {
+    if (window.innerWidth < 960) {
+      setRewardsDropdown(false);
+    } else {
+      setRewardsDropdown(false);
+    }
+  };
 
   return (
     <>
-      <div className="navbar">
-        <Link to="/" className="logo">
-          Logo
+      <nav className="navbar">
+        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+          Sustainable lifestyle dashboard
         </Link>
         <div className="menu-icon" onClick={handleClick}>
-          <i className={menuOpened ? "fas fa-times" : "fas fa-bars"} />
+          <i className={click ? "fas fa-times" : "fas fa-bars"} />
         </div>
-      </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li
+            className="nav-item"
+            onMouseEnter={onMouseChallengesEnter}
+            onMouseLeave={onMouseChallengesLeave}
+          >
+            <Link
+              to="/challenges"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Challenges <i className="fas fa-caret-down" />
+            </Link>
+            {challengesDropdown && <ChallengesDropdown />}
+          </li>
+          <li
+            className="nav-item"
+            onMouseEnter={onMouseRewardsEnter}
+            onMouseLeave={onMouseRewardsLeave}
+          >
+            <Link to="/rewards" className="nav-links" onClick={closeMobileMenu}>
+              Rewards <i className="fas fa-caret-down" />
+            </Link>
+            {rewardsDropdown && <RewardsDropdown />}
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
     </>
   );
 }
