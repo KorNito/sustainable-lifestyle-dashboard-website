7ddb0bfa02b79332a226b2c92b5e8c016b01161b
KorNito <kornelijuspacevicius@gmail.com>
2022-05-11 15:02:30 +0300

    Added links to challanges dropdown

---
+++ src/components/ChallengesLinks.js
@@ -0,0 +1,12 @@
+export const ChallengesLinks = [
+  {
+    title: "Create challenge",
+    path: "/create-challenge",
+    className: "dropdown-link",
+  },
+  {
+    title: "View challenges",
+    path: "/view-challenges",
+    className: "dropdown-link",
+  },
+];
