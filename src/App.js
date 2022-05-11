7f45c3fa05273907917332bc1d6426803000d09a
KorNito <kornelijuspacevicius@gmail.com>
2022-05-11 15:49:19 +0300

    Added routes to pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./components/Pages/Home";
-import Challenges from "./components/Pages/Challenges";
-import Rewards from "./components/Pages/Rewards";
+import Layout from "./components/Navbar";
+import Home from "./components/pages/Home";
+import Challenges from "./components/pages/Challenges";
+import Rewards from "./components/pages/Rewards";
+import NoPage from "./components/pages/NoPage";
+import CreateChallenge from "./components/pages/CreateChallenge";
+import ViewChallenges from "./components/pages/ViewChallenges";
+import CreateReward from "./components/pages/CreateReward";
+import ViewRewards from "./components/pages/ViewRewards";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path="/" exact component={Home} />
-        <Route path="/challenges" exact component={Challenges} />
-        <Route path="/rewards" exact component={Rewards} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="challenges" element={<Challenges />} />
+          <Route path="rewards" element={<Rewards />} />
+          <Route path="create-challenge" element={<CreateChallenge />} />
+          <Route path="view-challenges" element={<ViewChallenges />} />
+          <Route path="create-reward" element={<CreateReward />} />
+          <Route path="view-rewards" element={<ViewRewards />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
