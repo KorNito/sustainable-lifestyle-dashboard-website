0be9d92c13bd13e6e202d96053b0444d16be6a00
KorNito <kornelijuspacevicius@gmail.com>
2022-05-11 15:27:47 +0300

    Challenges drowdown link refactoring

---
+++ src/components/ChallengesDropdown.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { ChallengesLinks } from "./ChallengesLinks";
+import "./Dropdown.css";
+import { Link } from "react-router-dom";
+
+function ChallengesDropdown() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  return (
+    <>
+      <ul
+        onClick={handleClick}
+        className={click ? "dropdown-menu clicked" : "dropdown-menu"}
+      >
+        {ChallengesLinks.map((link, index) => {
+          return (
+            <li key={index}>
+              <Link
+                className={link.className}
+                to={link.path}
+                onClick={() => setClick(false)}
+              >
+                {link.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default ChallengesDropdown;
