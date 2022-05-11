8c91ac436d5965bfe309e2e41684265ce69ea607
KorNito <kornelijuspacevicius@gmail.com>
2022-05-11 15:25:35 +0300

    Added rewards dropdown links

---
+++ src/components/RewardsDropdown.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { RewardsLinks } from "./RewardsLinks";
+import "./Dropdown.css";
+import { Link } from "react-router-dom";
+
+function RewardsDropdown() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  return (
+    <>
+      <ul
+        onClick={handleClick}
+        className={click ? "dropdown-menu clicked" : "dropdown-menu"}
+      >
+        {RewardsLinks.map((link, index) => {
+          return (
+            <li key={index}>
+              <Link
+                className={link.className}
+                to={link.path}
+                onClick={() => setClick(false)}
+              >
+                {link.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default RewardsDropdown;
---
+++ src/components/RewardsLinks.js
@@ -0,0 +1,12 @@
+export const RewardsLinks = [
+  {
+    title: "Create reward",
+    path: "/create-reward",
+    className: "dropdown-link",
+  },
+  {
+    title: "View rewards",
+    path: "/view-rewards",
+    className: "dropdown-link",
+  },
+];
