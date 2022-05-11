0d56da2e7dc3f07d29662535417a8a13be85accf
KorNito <kornelijuspacevicius@gmail.com>
2022-05-11 10:10:36 +0300

    Added routes to page paths

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/Pages/Home";
+import Challenges from "./components/Pages/Challenges";
+import Rewards from "./components/Pages/Rewards";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar></Navbar>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact component={Home} />
+        <Route path="/challenges" exact component={Challenges} />
+        <Route path="/rewards" exact component={Rewards} />
+      </Routes>
     </BrowserRouter>
   );
 }
