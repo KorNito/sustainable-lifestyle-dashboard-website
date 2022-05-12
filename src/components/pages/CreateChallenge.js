1bc1839832b544c1bacf757514cef24f7ab1b22a
KorNito <kornelijuspacevicius@gmail.com>
2022-05-12 20:05:07 +0300

    Create Challenges form

--- src/components/pages/CreateChallenge.js
+++ src/components/pages/CreateChallenge.js
@@ -1,7 +1,143 @@
-import React from "react";
+import React, { useState } from "react";
+import "./CreateChallenge.css";
+import useInput from "../../hooks/use-input";
+
+const isNotEmpty = (value) => value.trim() !== "";
 
 const CreateChallenge = () => {
-  return <div>CreateChallenge</div>;
+  const [isSending, setIsSending] = useState(false);
+
+  const {
+    value: challengeNameValue,
+    isValid: challengeNameIsValid,
+    hasError: challengeNameHasError,
+    valueChangeHandler: challengeNameChangeHandler,
+    inputBlurHandler: challengeNameBlurHandler,
+    reset: resetChallengeName,
+  } = useInput(isNotEmpty);
+
+  const {
+    value: pointsValue,
+    isValid: pointsIsValid,
+    hasError: pointsHasError,
+    valueChangeHandler: pointsChangeHandler,
+    inputBlurHandler: pointsBlurHandler,
+    reset: resetPoints,
+  } = useInput(isNotEmpty);
+
+  const {
+    value: startDateValue,
+    isValid: startDateIsValid,
+    hasError: startDateHasError,
+    valueChangeHandler: startDateChangeHandler,
+    inputBlurHandler: startDateBlurHandler,
+    reset: resetStartDate,
+  } = useInput(isNotEmpty);
+
+  const {
+    value: endDateValue,
+    isValid: endDateIsValid,
+    hasError: endDateHasError,
+    valueChangeHandler: endDateChangeHandler,
+    inputBlurHandler: endDateBlurHandler,
+    reset: resetEndDate,
+  } = useInput(isNotEmpty);
+
+  let formIsValid = false;
+
+  if (
+    challengeNameIsValid &&
+    pointsIsValid &&
+    startDateIsValid &&
+    endDateIsValid
+  ) {
+    formIsValid = true;
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (!formIsValid) {
+      return;
+    }
+
+    setIsSending(true);
+
+    fetch(
+      "https://sustainable-lifestyle-30c7b-default-rtdb.europe-west1.firebasedatabase.app/challenges.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          challengeName: challengeNameValue,
+          points: pointsValue,
+          startDate: startDateValue,
+          endDate: endDateValue,
+        }),
+      }
+    );
+
+    setIsSending(false);
+
+    resetChallengeName();
+    resetPoints();
+    resetStartDate();
+    resetEndDate();
+  };
+
+  return (
+    <div className="createChallengeContainer">
+      <form onSubmit={submitHandler}>
+        <h1>Create a challenge</h1>
+        <label htmlFor="name">Challenge name</label>
+        <input
+          type="text"
+          id="name"
+          value={challengeNameValue}
+          onChangeCapture={challengeNameChangeHandler}
+          onBlur={challengeNameBlurHandler}
+        />
+        {challengeNameHasError && <p>Please enter challenge name</p>}
+
+        <label htmlFor="points">Points</label>
+        <input
+          type="text"
+          id="points"
+          value={pointsValue}
+          onChangeCapture={pointsChangeHandler}
+          onBlur={pointsBlurHandler}
+        />
+        {pointsHasError && <p>Please enter points</p>}
+
+        <label htmlFor="startDate">Start date</label>
+        <input
+          type="text"
+          id="startDate"
+          value={startDateValue}
+          onChangeCapture={startDateChangeHandler}
+          onBlur={startDateBlurHandler}
+        />
+        {startDateHasError && <p>Please enter start date</p>}
+
+        <label htmlFor="endDate">End date</label>
+        <input
+          type="text"
+          id="endDate"
+          value={endDateValue}
+          onChangeCapture={endDateChangeHandler}
+          onBlur={endDateBlurHandler}
+        />
+        {endDateHasError && <p>Please enter end date</p>}
+
+        {isSending ? (
+          <button disabled>Creating challenge...</button>
+        ) : (
+          <button disabled={!formIsValid} type="Submit">
+            Submit
+          </button>
+        )}
+      </form>
+    </div>
+  );
 };
 
 export default CreateChallenge;
